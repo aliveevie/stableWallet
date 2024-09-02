05bb94a5c1e31dcdc60f098ac409a94a43231918
Ibrahim <aliveibi080@gmail.com>
2024-09-02 07:58:31 +0100

    Create the view

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
-import Image from "next/image";
+import styles from './styles/Mobile.module.css'
+
 
 export default function Home() {
   return (
-   
     <>
-      <h1>Hello World!</h1>
+      <div className={styles.container} >
+    
+      </div>
     </>
     
   );
