275c49adeea30b74da5961c927277ec8f4f85535
Ibrahim <aliveibi080@gmail.com>
2024-09-02 10:15:28 +0100

    Create Header Component

---
+++ app/components/Header.js
@@ -0,0 +1,16 @@
+import styles from '../styles/Header.module.css';
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.menuIcon}></div>
+      <div className={styles.userInfo}>
+        <div className={styles.username}>Ibrahim Abdulkarim</div>
+        <div>(evletwo.near)</div>
+      </div>
+      <div className={styles.latency}>1716ms</div>
+    </div>
+  );
+};
+
+export default Header;
